0ff97208774fd7110ab24435662771b4506fa119
ZachB1121 <zacharybrown.student15@bpsne.net>
2014-06-27 10:34:09 -0500

    Added Format Date function
    The new format date function pulls the date from the menu items to allow
    for a multiday menu.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 App = Ember.Application.create();
 console.log("Run!");
 //var posts = parseJSON(JSONData); //See bottom
-
+var bigDate;
 
 
 App.Router.map(function() {
@@ -46,19 +46,32 @@ App.itemController = Ember.ObjectController.extend({
 });
 
 Ember.Handlebars.helper('format-date', function(date) {
-  return moment(date).toLocaleDateString();
+  return bigDate.toLocaleDateString();
 });
 
 
-var JSONString = '{"1":{"title":"Hamburger","author":{"name":"Zach"},"excerpt":"A tasty burger","body":"A delicious burger made of well... burger. 100 Cal. $17.99"},"2":{"title":"Cheeseburger","author":{"name":"Zach"},"excerpt":"A tasty cheese burger","body":"A delicious burger made of well... burger.. oh an Cheese!. 100 Cal. $17.99"},"3":{"title":"Veggiburger","author":{"name":"Zach"},"excerpt":"A tasty veggiburger","body":"A delicious burger made of well... plants. 100 Cal. $17.99"},"4":{"title":"Chicken Fingers","author":{"name":"Charlie"},"excerpt":"A Yummy stuff","body":"A chicken."},"date":"06-15-2014"}';
+var JSONString = '{"1":{"title":"Hamburger","restaurant":{"name":"Charlie\'s"},"date":"06-15-2014","excerpt":"A tasty burger","body":"A delicious burger made of well... burger. 100 Cal. $17.99"},"2":{"title":"Cheeseburger","restaurant":{"name":"Charlie\'s"},"date":"06-15-2014","excerpt":"A tasty cheese burger","body":"A delicious burger made of well... burger.. oh an Cheese!. 100 Cal. $17.99"},"3":{"title":"Veggiburger","restaurant":{"name":"Charlie\'s"},"date":"06-15-2014","excerpt":"A tasty veggiburger","body":"A delicious burger made of well... plants. 100 Cal. $17.99"},"4":{"title":"Chicken Fingers","restaurant":{"name":"Charlie"},"date":"06-15-2014","excerpt":"A Yummy stuff","body":"A chicken."}}';
+
+//This one has dates attached to each food items instead of all of them
+
+//var JSONString = '{"1":{"title":"Hamburger","restaurant":{"name":"Charlie\'s"},"date":"06-15-2014","excerpt":"A tasty burger","body":"A delicious burger made of well... burger. 100 Cal. $17.99"},"2":{"title":"Cheeseburger","restaurant":{"name":"Charlie\'s"},"date":"06-15-2014","excerpt":"A tasty cheese burger","body":"A delicious burger made of well... burger.. oh an Cheese!. 100 Cal. $17.99"},"3":{"title":"Veggiburger","restaurant":{"name":"Charlie\'s"},"date":"06-15-2014","excerpt":"A tasty veggiburger","body":"A delicious burger made of well... plants. 100 Cal. $17.99"},"4":{"title":"Chicken Fingers","author":{"name":"Charlie"},"date":"06-15-2014","excerpt":"A Yummy stuff","body":"A chicken."}}'; 
 
 var parseJSON = function (){
 
  var temp = [];
  //var JSONValue = $.getJSON("./JSON.html"); //From Web
  var JSONValue = $.parseJSON(JSONString);
+ var indvDate = JSONValue["date"] === 'undefined';
+ 
+ if( ! indvDate ){ //If one big date
+	bigDate = new Date(JSONValue["date"]);
+	//bigDate = new Date();
+ } else {
+    bigDate = new Date();
+ }
  
  //console.log("data: "+JSON.stringify(JSONValue, null, 4));
+ 
  for (var postKey in JSONValue){
   var slot = temp.length;
   if(postKey === "date"){
@@ -68,7 +81,7 @@ var parseJSON = function (){
   temp[slot].id = parseInt(postKey);
   temp[slot].title = JSONValue[postKey].title;
   temp[slot].restaurant = JSONValue[postKey].restaurant; 
-  temp[slot].date = new Date(JSONValue["date"]); 
+  temp[slot].date = indvDate ? new Date(JSONValue[postKey].date) : new Date(JSONValue["date"]); 
   temp[slot].excerpt = JSONValue[postKey].excerpt; 
   temp[slot].body = JSONValue[postKey].body;   
  }
