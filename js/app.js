4507bf93d600330c2cfc68f50fbcff986b668841
Charlie H-R <charleshansen-reed@creighton.edu>
2014-06-30 11:25:56 -0500

    Trimmed the fat
    got rid of some whitespace/comments/unnecessary console.logs because
    they were bothering me. maybe did some other stuff too, I can't
    remember.

--- js/app.js
+++ js/app.js
@@ -66,20 +66,13 @@ App.Menu2Controller = Ember.ObjectController.extend({
 
   next: function() {
       bigDate.setDate(bigDate.getDate() + 1);
-      console.log("changed +");
-	 // $('#dateDisplay').html('<a href="#/menu2/date">'+getDate()+'</a>');
 	  $('#dateDisplay').html(getDate());
-      console.log(getDate());
       parseJSON();
-      console.log(items);
   },
 
   prev: function() {
       bigDate.setDate(bigDate.getDate() - 1);
-      console.log("changed -");
-	  //$('#dateDisplay').html('<a href="#/menu2/date">'+getDate()+'</a>');
 	  $('#dateDisplay').html(getDate());
-      console.log(getDate());
       parseJSON();
       console.log(items);
   }
@@ -91,7 +84,6 @@ Ember.Handlebars.helper('format-date', function(temp) {
 
 Ember.Handlebars.helper('get-date', getDate); //This needs to be a function! JK
 
-
 var getDate = function getDate(){
   return bigDate.toLocaleDateString();
 }
@@ -102,9 +94,8 @@ var JSONString = ['{"date":"06-29-2014","1":{"title":"Hamburger-1?!","restaurant
                   '{"date":"07-01-2014","1":{"title":"Hamburger1?!","restaurant":{"name":"Charlie\'s"},"excerpt":"A tasty burger","body":"A delicious burger made of well... burger. 100 Cal. $17.99"},"2":{"title":"Cheeseburger?!","restaurant":{"name":"Charlie\'s"},"excerpt":"A tasty cheese burger","body":"A delicious burger made of well... burger.. oh an Cheese!. 100 Cal. $17.99"},"3":{"title":"Veggiburger?!","restaurant":{"name":"Charlie\'s"},"excerpt":"A tasty veggiburger","body":"A delicious burger made of well... plants. 100 Cal. $17.99"},"4":{"title":"Chicken Fingers?!","restaurant":{"name":"Charlie"},"excerpt":"A Yummy stuff","body":"A chicken."},"5":{"title":"Chicken Fingers2?!","restaurant":{"name":"Charlie"},"excerpt":"A Yummy stuff","body":"A chicken."},"6":{"title":"Chicken Fingers3?!","restaurant":{"name":"Charlie"},"excerpt":"A Yummy stuff","body":"A chicken."}}'];
 
 
-
 var parseJSON = function (){
-    if(typeof bigDaate === 'undefined'){ //No current date.. set it to today
+    if(typeof bigDate === 'undefined'){ //No current date.. set it to today
     bigDate = new Date();
   } else {
     //Do nothing.
@@ -123,13 +114,9 @@ var parseJSON = function (){
   }
  }
  
- //var JSONValue = $.getJSON("./JSON.html"); //From Web
  var JSONValue = $.parseJSON(JSONString[index]);
  var indvDate = typeof JSONValue["date"] === 'undefined';
  
- //console.log("data: "+JSON.stringify(JSONValue, null, 4));
- 
-
  
  for (var postKey in JSONValue){
   var slot = temp.length;
@@ -145,7 +132,6 @@ var parseJSON = function (){
   temp[slot].body = JSONValue[postKey].body;   
  }
 
- 
  
  console.log("temp: ");
  console.log(temp);
